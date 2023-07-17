d12b9cc1fdabea209c1ce36781aaf5f1ef0ebc1a
GabrielRsiqueira18 <gabrielrsiqueira0180@gmail.com>
2023-07-17 13:18:56 -0300

    feat: create BankRepository and Interface, for now all is finish

--- src/repositories/interfaces/InterfaceBank.java
+++ src/repositories/interfaces/InterfaceBank.java
@@ -1,5 +1,9 @@
 package repositories.interfaces;
 
+import entities.Bank;
+
 public interface InterfaceBank {
-	
+	public Bank getBankByName(String name);
+	public Bank create(String name);
+	public void listAllBanks();
 }
